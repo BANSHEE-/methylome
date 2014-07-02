a0e7ffa0d5d57ca7bf1618165269401a7ab795d2
blanche <blanche@darkprism.(none)>
2014-07-01 20:57:35 -0500

    determine data version function works. next: ver a dis function. fix output list issue. write discretized list into output file

--- distest.py
+++ distest.py
@@ -1,38 +1,86 @@
 import os
 import glob
 
-## list of directories
-
+## list of directories#########
+#still would like to set this up using os.abspath instead, talk to dakota/claus
 data_dir = "/home/blanche/Desktop/wilke/DNA_Methylation/JHU_USC__HumanMethylation27/Level_3"
 
 working_dir ="/home/blanche/Desktop/wilke/DNA_Methylation/JHU_USC__HumanMethylation27"
 
 ## functions
 
-def determine_file_type( file ):
+def determine_file_type( data_fh ):
 
-  type = 0
-
-  return type
-
-def discretize_A( file, newlines ):
+  for line in data_fh:
+    line = line.strip( '\r\n' )
+    fields = line.split( '\t' )
+    if fields[0] == "barcode":
+      ver = "A" #why did dakota suggest *type* in this instance?
+      return ver
+    elif fields[0] == "Hybridization REF":
+      ver = "B"
+      return ver
+    break 
+    #break so that the loop will end after only one line iteration through the file
+    
+def discretize_A( data_fh, outlist ):
   
   print "Function A not yet written."
 
   return
   
-def discretize_B( file, newlines ):
+def discretize_B( data_fh, outlist ):
+  for line in data_fh:
+  # strip the line of whitespace characters and separate by tabs is this redundant!!
+    line = line.strip( '\r\n' )
+    fields = line.split( '\t' )
 
-  print "Function B not yet written."
 
-  return
+    ref = fields[ 0 ]
+  
+
+    level = "NA"  
+    beta = fields[ 1 ]
+#continue goes to next iteration of the loop to skip the headers
+    if beta[0:5] == "TCGA":
+    #header differs in each file. index string to check for first 4 chr TCGA
+      print "TCGA"
+    if beta == "Beta_value":
+      continue
+#now we have gotten past ver b header
+    if beta != "NA":
+  
+      beta = float(beta)
+      
+      if beta <= 0.2:
+        level = 0
+      elif beta >= 0.6:
+        level = 2
+      else:
+        level = 1
+  
+  gene = fields[ 2 ]
+  chrome = fields[ 3 ]
+  gencoord = fields[ 4 ]
+
+  # 6. write newline
+  disline = "%s\t%s\t%s\t%s\t%s\n" % ( ref, level, gene, chrome, gencoord )
+  #return of the function
+  outlist.append(disline)
+  
+
+  
+  
+  
+  
+  
 
 
 ###################
 ## Main function ##
 ###################
 
-newlines =[]
+outlist =[]
 
 # 1. access all files in a directory with os
 files =[]
@@ -42,7 +90,7 @@ for file in glob.glob( "*.txt" ):
   files.append( filename )
 os.chdir( working_dir )
   
-print files
+
 
 
 # 2. iterate through the level 3 directory
@@ -50,22 +98,27 @@ for file in files:
   # 3. open data file
   data_fh = open(file, "r")
   # determine if the file is type A or type B
-  type = determine_file_type( file )    
+  ver = determine_file_type( data_fh )    
   # run the code to discretize beta values, according to type
-  if type == "A":
+  if ver == "A":
     # do A function
-    discretize_A( file, newlines )
-  elif type == "B":
-    discretize_B( file, newlines )
+    discretize_A( data_fh , outlist )
+  elif ver == "B":
+    discretize_B( data_fh , outlist )
   else:
-    print "ERROR! invalid file type"
+    print "ERROR >___<"
+  
+print outlist #only getting last line of files ... hmmmm ...
 
+
+
+
+'''
 
   print type
 
 
 
-'''
       # 4. seperate and strip
       for line in data:
         line = line.strip( '\r\n' )
